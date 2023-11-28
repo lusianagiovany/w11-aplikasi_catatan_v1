64a5b1c15e23abfc7826d294d6bfe74a71fe3cdf
lusianagiovany <142966457+lusianagiovany@users.noreply.github.com>
2023-11-28 23:31:36 +0700

    update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
 import AddTodo from './pages/AddTodo';
 import HomePage from './pages/HomePage';
-// import React, { useState } from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 
 function App() {
-  // const [todos, setTodos] = useState([]);
-
-  // const addTodo = (newTodo) => {
-  //   setTodos([...todos, newTodo]);
-  // };
-  
   return (
     <BrowserRouter>
     <Routes>
